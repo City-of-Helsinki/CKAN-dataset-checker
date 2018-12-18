0876e17af186987b32bf26f5fee13cc9a36263c5
Henri Kotkanen <henri.kotkanen@hel.fi>
2018-12-18 18:18:42 +0200

    Bring checkers up-to-date with CKAN version used in hri.fi

--- check_hri_brokenlinks.py
+++ check_hri_brokenlinks.py
@@ -1,13 +1,14 @@
 # encoding: utf-8
 import os
 import sys
-from urllib2 import urlopen
-import urllib2
-import urlparse
+from urllib.request import urlopen
+import urllib.request, urllib.error, urllib.parse
+import urllib.parse
 import json
+import socket
 
 # From the werkzeug-project: https://github.com/mitsuhiko/werkzeug/blob/master/werkzeug/urls.py
-import urllib
+import urllib.request, urllib.parse, urllib.error
 def url_fix(s, charset='utf-8'):
     """Sometimes you get an URL by a user that just isn't a real
     URL because it contains unsafe characters like ' ' and so on.  This
@@ -20,12 +21,12 @@ def url_fix(s, charset='utf-8'):
     :param charset: The target charset for the URL if the url was
                     given as unicode string.
     """
-    if isinstance(s, unicode):
-        s = s.encode(charset, 'ignore')
-    scheme, netloc, path, qs, anchor = urlparse.urlsplit(s)
-    path = urllib.quote(path, '/%')
-    qs = urllib.quote_plus(qs, ':&=')
-    return urlparse.urlunsplit((scheme, netloc, path, qs, anchor))
+#    if isinstance(s, str):
+#        s = s.encode(charset, 'ignore')
+    scheme, netloc, path, qs, anchor = urllib.parse.urlsplit(s)
+    path = urllib.parse.quote(path, '/%')
+    qs = urllib.parse.quote_plus(qs, ':&=')
+    return urllib.parse.urlunsplit((scheme, netloc, path, qs, anchor))
 
 def iterate_resources(packages):
     for package in packages:
@@ -35,14 +36,14 @@ def iterate_resources(packages):
 
 def load_metadata(url):
     metadata = json.load(urlopen(url))
-    packages = metadata['packages']
+    packages = metadata['result']
     return list(iterate_resources(packages))
 
 def check_links(outfile='notfound.txt', metadata_url=None):
     if not metadata_url:
         return None
 
-    # print "Checking for broken links from " + metadata_url
+    print("Checking for broken links from " + metadata_url)
     notfound_count = 0
 
     resources = load_metadata(metadata_url)
@@ -53,21 +54,29 @@ def check_links(outfile='notfound.txt', metadata_url=None):
             # print 'Checking broken link for ', resource['url']
             data = urlopen(resource['url'], timeout=5)
 
-        except urllib2.HTTPError, e:
+        except urllib.error.HTTPError as e:
             # print >>sys.stderr, e, 'at', resource['url']
-            notfound.write(e.reason + ' at ' + resource['url'] + ', from ' + resource['package']['ckan_url'] + '\n')
+            #import pprint;
+            #pprint.pprint(resource)
+            notfound.write(e.reason + ' at ' + resource['url'] + ', from ' + resource['package']['id'] + '\n')
             notfound_count += 1
             continue
 
-        except urllib2.URLError, e:
+        except urllib.error.URLError as e:
             # print >>sys.stderr, e, 'at', resource['url']
-            notfound.write('Something went wrong at ' + resource['url'] + ', from ' + resource['package']['ckan_url'] + '\n')
+            notfound.write('Something went wrong at ' + resource['url'] + ', from ' + resource['package']['id'] + '\n')
             notfound_count += 1
             continue
 
-        except Exception, e:
+        except socket.timeout as e:
+            notfound.write(f"Connection timed out at {resource['url']}, from {resource['package']['id']}\n")
+            continue
+
+        except Exception as e:
             # print >>sys.stderr, e, 'at', resource['url']
-            notfound.write('Error: ' + e.message + ', at ' + resource['url'] + ', from ' + resource['package']['ckan_url'] + '\n')
+            print(vars(e))
+            print(type(e))
+            notfound.write(f"Error: {e}, at {resource['url']}, from {resource['package']['id']}\n")
             notfound_count += 1
             continue
 
--- check_hri_expired.py
+++ check_hri_expired.py
@@ -13,30 +13,40 @@ def check_expired(outfile='expired.txt', metadata_url=None):
 
     # print "Checking for expired datasets from " + metadata_url
     data = requests.get(metadata_url).json()
-    packages = data['packages']
+    packages = data['result']
 
     with codecs.open(outfile, 'w', encoding='utf8') as reportfile:
 
         reportfile.write('url,date_updated,update_frequency\n')
         expired_count = 0
         for package in packages:
-            # check if packages have a specified update frequency and whether they have been updated in the last year
-            if (package['extras']['update_frequency']) and (package['extras']['date_updated']):
-
+            # if package has update information we check do a simple heuristic check
+            # and see if has been updated during the last 365 days
+            if 'date_updated' in package:
                 try:
                     # try in format: 2013-11-10
-                    updated = datetime.datetime.strptime(package['extras']['date_updated'], '%Y-%m-%d')
+                    updated = datetime.datetime.strptime(package['date_updated'], '%Y-%m-%d')
                 except:
                     # should be in other format: 10/11/2014
                     try:
-                        updated = datetime.datetime.strptime(package['extras']['date_updated'], '%d/%m/%Y')
+                        updated = datetime.datetime.strptime(package['date_updated'], '%d/%m/%Y')
                     except: # we never know what the date format is like, huh
-                        updated = datetime.datetime.strptime(package['extras']['date_updated'], '%m/%d/%Y')
+                        updated = datetime.datetime.strptime(package['date_updated'], '%m/%d/%Y')
 
                 finally:
                     # check whether the last update was over a year ago
                     if updated and ((datetime.datetime.now() - updated).days) > 365:
-                        reportfile.write(package['ckan_url']+','+package['extras']['date_updated']+','+package['extras']['update_frequency']+'\n')
-                        expired_count += 1
+                        try:
+                            try:
+                                update_frequency = package['update_frequency']['fi']
+                            except KeyError as e:
+                                update_frequency = "update frequency not defined"
+                            reportfile.write("{},{},{}\n".format(package['id'], package['date_updated'], update_frequency))
+                            expired_count += 1
+                        except KeyError as e:
+                            print("Data record misshapen while reading update_frequency:")
+                            print(e.args)
+                            print(package)
+
 
         return expired_count
