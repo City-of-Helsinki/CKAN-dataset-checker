1838aa1e1122bac4f64e1494a0173643e72bffe6
Henri Kotkanen <henri.kotkanen@hel.fi>
2018-12-18 18:18:17 +0200

    Use smptlib and email for sending message

--- run_hri_checks.py
+++ run_hri_checks.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from check_hri_brokenlinks import check_links
 from check_hri_expired import check_expired
-from sendmail import send_mail
+from email.message import EmailMessage
+from email.policy import SMTP
+import smtplib
 import configparser
 
 conf = configparser.ConfigParser()
@@ -13,7 +15,7 @@ NOTFOUND_OUTFILE_NAME = conf['DEFAULT']['notfound_outfile_name']
 EXPIRED_OUTFILE_NAME = conf['DEFAULT']['expired_outfile_name']
 
 # mail configuration
-#to-list is expected to be comma separated values of the form "Name <email@addre.ss>"
+# to-list is expected to be comma separated values of the form "Name <email@addre.ss>"
 RECIPIENT_LIST = conf['DEFAULT']['recipient_list'].split(',')
 SENDER = conf['DEFAULT']['sender']
 SERVER = conf['DEFAULT']['server']
@@ -28,5 +30,17 @@ if __name__=='__main__':
     expired_count = check_expired(EXPIRED_OUTFILE_NAME, METADATA_URL)
     # print 'Expired: ', expired_count
 
-    # print 'Sending mail'
-    send_mail(RECIPIENT_LIST, SENDER, TITLE, BODY.format(notfound_count, expired_count), files=[NOTFOUND_OUTFILE_NAME, EXPIRED_OUTFILE_NAME], server='localhost')
+    msg = EmailMessage()
+    msg['Subject'] = TITLE
+    msg['From'] = SENDER
+    msg['To'] = RECIPIENT_LIST
+
+    msg.set_content(BODY.format(notfound_count, expired_count))
+
+    for file in (NOTFOUND_OUTFILE_NAME, EXPIRED_OUTFILE_NAME):
+        with open(file, "rb") as fp:
+            report_data = fp.read()
+        msg.add_attachment(report_data, maintype="text", subtype="plain")
+
+    with smtplib.SMTP('localhost') as s:
+        s.send_message(msg)
